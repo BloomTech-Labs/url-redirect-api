b61e6c2ae880ed58c28f2357143640ffd59a6822
ashtilawat23 <ashtilawat23@gmail.com>
2023-07-25 14:30:48 -0400

    removed init in app.py

--- app/api.py
+++ app/api.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import MongoDB
-from app.validation import User, UserQuery, UserUpdate
-from app.validation import default_query, default_update, default_user
 
 
 API = FastAPI(
-    title="BloomTech Labs DS API Template",
+    title="BloomTech Labs URL Redirect API",
     version="0.0.1",
     docs_url="/",
 )
@@ -27,39 +25,3 @@ async def api_version():
     @return: String Version """
     return API.version
 
-
-@API.post("/create-user")
-async def create_user(user: User = default_user):
-    """ Creates one user
-    @param user: User
-    @return: Boolean Success """
-    return API.db.create(user.dict(exclude_none=True))
-
-
-@API.put("/read-users")
-async def read_users(user_query: UserQuery = default_query):
-    """ Returns array of all matched users
-    @param user_query: UserQuery
-    @return: Array[User] """
-    return API.db.read(user_query.dict(exclude_none=True))
-
-
-@API.patch("/update-users")
-async def update_users(user_query: UserQuery = default_query,
-                       user_update: UserUpdate = default_update):
-    """ Updates all matched users
-    @param user_query: UserQuery
-    @param user_update: UserUpdate
-    @return: Boolean Success """
-    return API.db.update(
-        user_query.dict(exclude_none=True),
-        user_update.dict(exclude_none=True),
-    )
-
-
-@API.delete("/delete-users")
-async def delete_users(user_query: UserQuery = default_query):
-    """ Deletes all matched users
-    @param user_query: UserQuery
-    @return: Boolean Success """
-    return API.db.delete(user_query.dict(exclude_none=True))
